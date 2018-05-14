431517f0b6257defd1a5a5692abdaf9b2df32d3e
sadiquefarooqui@gmail.com <sadiquefarooqui@gmail.com>
2018-05-14 17:56:39 -0600

    removing client

--- platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
+++ platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-@EnableEurekaClient
+
 @EnableEurekaServer
 @SpringBootApplication
 public class App {
